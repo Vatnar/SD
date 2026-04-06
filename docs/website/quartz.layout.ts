e0a6985df0d4a26aa47da10f6e170eedf0bf6a14
Peter Vatnar Andersson <Peter@70x7.net>
2026-04-06 22:30:15 +0200

    Move website folder and add topbar

--- docs/website/quartz.layout.ts
+++ docs/website/quartz.layout.ts
@@ -4,7 +4,15 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.NavLinks({
+      links: {
+        Home: "/",
+        "API Docs": "/docs/",
+        "GitHub Repo": "https://github.com/Vatnar/SD",
+      },
+    }),
+  ],
   afterBody: [],
   footer: Component.Footer({
     links: {
